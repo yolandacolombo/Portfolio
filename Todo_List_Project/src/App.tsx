3ad83be304da32e5a9d6d3b6148cad698ac03ef2
Yolanda Colombo <yoocolomboo@gmail.com>
2025-10-15 21:34:36 -0300

    Todo List Project

---
+++ Todo_List_Project/src/App.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+
+// components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import Modal from './components/Modal'
+
+//CSS
+import styles from "./App.module.css";
+
+// Interface
+import { ITask } from "./interfaces/Task";
+
+function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter(task => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if(display) {
+      modal!.classList.remove("hide");
+    }else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+
+    const updatedTask: ITask = {id, title, difficulty};
+
+    const updatedItems = taskList.map((task) => { // map é um loop para atualizar alguma coisa
+      return task.id === updatedTask.id ? updatedTask : task;
+    })
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  }
+
+  return (
+    <div >
+      <Modal 
+        children={
+          <TaskForm 
+            btnText='Editar Tarefa' 
+            taskList={taskList} 
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      />
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm 
+            btnText='Criar Tarefa' 
+            taskList={taskList} 
+            setTaskList={setTaskList}
+          />
+        </div>
+        <div> 
+          <h2>Suas tarefas:</h2>
+          <TaskList 
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Todo_List_Project/src/components/TaskForm.tsx
@@ -0,0 +1,83 @@
+import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react'
+
+//CSS
+import styles from "./TaskForm.module.css";
+
+// Interface
+import { ITask } from "../interfaces/Task";
+
+type Props = {
+    btnText: string;
+    taskList: ITask[];
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
+    task?: ITask | null;
+    handleUpdate?(id: number, title: string, difficulty: number): void;
+}
+
+const TaskForm = ( {btnText, taskList, setTaskList, task, handleUpdate}: Props) => {
+  const [id, setId] = useState<number>(0);
+  const [title, setTitle] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<number>(0);
+
+  useEffect(() => {
+    if(task) {
+      setId(task.id);
+      setTitle(task.title);
+      setDifficulty(task.difficulty);
+    }
+  }, [task]);
+
+  const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(handleUpdate) {
+      handleUpdate(id, title, difficulty);
+
+    } else {
+      const id = Math.floor(Math.random() * 1000)
+
+      const newTask: ITask = {id, title, difficulty}
+
+      setTaskList!([...taskList, newTask]);
+      
+      setTitle("");
+      setDifficulty(0);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "title") {
+      setTitle(e.target.value)
+    }else {
+      setDifficulty(parseInt(e.target.value))
+    }
+  }
+
+  return (
+    <form onSubmit={addTaskHandler} className={styles.form}>
+        <div className={styles.input_container}>
+            <label htmlFor="title">Título: </label>
+            <input 
+              type="text" 
+              name="title" 
+              placeholder="Título da terefa" 
+              onChange={handleChange}
+              value={title}
+            />
+        </div>
+        <div className={styles.input_container}>
+            <label htmlFor="title">Dificuldade: </label>
+            <input 
+              type="text" 
+              name="difficulty" 
+              placeholder="Dificuldade da Terefa"
+              onChange={handleChange}
+              value={difficulty}
+            />
+        </div>
+        <input type="submit" value={btnText} />
+    </form>
+  );
+};
+
+export default TaskForm
